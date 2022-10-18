84e55bf5b283e8032867cca102d41d2f734ff12b
Stefano Di Pasquale <stefaniscion@gmail.com>
2022-10-18 19:12:04 +0200

    22/94 completed

--- rustlings/exercises/quiz1.rs
+++ rustlings/exercises/quiz1.rs
@@ -10,10 +10,17 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples:i32) -> i32 {
+    let price:i32;
+    if apples > 40 {
+        price = apples * 1;
+    }else{
+        price = apples * 2;
+    }
+    price
+}
 
 // Don't modify this function!
 #[test]
